09ec8189d38552f71e2d9286fdc0479048845a34
joesan <codeintheopen@gmail.com>
2017-11-21 06:15:11 +0100

    added concurency

--- concurrency/main.go
+++ concurrency/main.go
@@ -10,5 +10,28 @@ import(
 var wg sync.WaitGroup
 
 func init() {
-    runtim.GOMAXPROCS(runtime.
+    runtim.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+    wg.Add(2)
+    go foo()
+    go bar()
+    wg.wait()
+}
+
+func foo() {
+    for i = 0; i < 100; i++ {
+        fmt.Println("Foo = ", i)
+        time.Sleep(time.Duration(4 * time.Millisecond))
+    }
+    wg.done()
+}
+
+func bar() {
+    for i = 0; i < 100; i++ {
+        fmt.Println("Boo = ", i)
+        time.Sleep(time.Duration(20 * time.Millisecond))
+    }
+    wg.done()
 }
